d078c8ebac51c30cab4c12c8a6ca4c9066460846
Akv1gn <oktavianoalvin1@gmail.com>
2025-04-16 07:06:58 +0700

    Tugas switch case update terbaru

--- sistem-pengecekan-ganjil-genap.js
+++ sistem-pengecekan-ganjil-genap.js
@@ -1,11 +1,11 @@
-let plat_nomer = ["L 1239 VGA"]
-let tanggal = 3;
+let plat_nomer = ["L 1238 VGA"]
+let tanggal = 5;
 let array = plat_nomer[0].split(" ")
 switch (true) {
-    case (array[1] % 2 == 0 && tanggal % 3 == 0):
-        console.log("maaf, anda harus saya tahan sebentar");
+    case (array[1] % 2 == 0 && tanggal % 2 == 0 || array[1] % 2 == 1 && tanggal % 2 == 1):
+        console.log("silahkan melanjutkan perjalanan anda");
         break;
     default:
-        console.log("silahkan melanjutkan perjalanan anda");
+        console.log("maaf, anda haru saya tahan sebentar");
         break;
 }
